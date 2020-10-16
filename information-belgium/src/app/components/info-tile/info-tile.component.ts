c0b6659952614c5254b2665cf63dd751b6cd6db4
Joel Camacho <joel.camacho@icometrix.com>
2020-10-17 00:48:32 +0200

    more components

--- information-belgium/src/app/components/info-tile/info-tile.component.ts
+++ information-belgium/src/app/components/info-tile/info-tile.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+export interface InfoTopic {
+  id: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-info-tile',
@@ -7,7 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoTileComponent implements OnInit {
 
-  constructor() { }
+  private infoTopics = [
+    {
+      id: '1',
+      image: 'src/assets/img/tiles/education.jpeg'
+    },
+    {
+      id: '2',
+      image: 'src/assets/img/tiles/healthcare.jpg'
+    },
+    {
+      id: '3',
+      image: 'src/assets/img/tiles/history.jpg'
+    },
+    {
+      id: '4',
+      image: 'src/assets/img/tiles/housing.jpg'
+    },
+    {
+      id: '5',
+      image: 'src/assets/img/tiles/politics.jpg'
+    },
+    {
+      id: '6',
+      image: 'src/assets/img/tiles/recycling.jpg'
+    },
+    {
+      id: '7',
+      image: 'src/assets/img/tiles/taxes.jpg'
+    },
+    {
+      id: '8',
+      image: 'src/assets/img/tiles/traditions.jpg'
+    },
+    {
+      id: '9',
+      image: 'src/assets/img/tiles/values.jpg'
+    },
+    {
+      id: '10',
+      image: 'src/assets/img/tiles/work.jpg'
+    },
+    {
+      id: '1',
+      image: 'src/assets/img/tiles/'
+    },
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
