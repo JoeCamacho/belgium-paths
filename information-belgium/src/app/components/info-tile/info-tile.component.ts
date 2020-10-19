633c1a2faa49267697e62fb81632a5462bbb50ca
Joel Camacho <joel.camacho@icometrix.com>
2020-10-19 22:10:01 +0200

    add grid images for sections

--- information-belgium/src/app/components/info-tile/info-tile.component.ts
+++ information-belgium/src/app/components/info-tile/info-tile.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 export interface InfoTopic {
   id: string;
@@ -12,52 +12,7 @@ export interface InfoTopic {
 })
 export class InfoTileComponent implements OnInit {
 
-  public infoTopics = [
-    {
-      id: '1',
-      image: 'src/assets/img/tiles/education.jpeg'
-    },
-    {
-      id: '2',
-      image: 'src/assets/img/tiles/healthcare.jpg'
-    },
-    {
-      id: '3',
-      image: 'src/assets/img/tiles/history.jpg'
-    },
-    {
-      id: '4',
-      image: 'src/assets/img/tiles/housing.jpg'
-    },
-    {
-      id: '5',
-      image: 'src/assets/img/tiles/politics.jpg'
-    },
-    {
-      id: '6',
-      image: 'src/assets/img/tiles/recycling.jpg'
-    },
-    {
-      id: '7',
-      image: 'src/assets/img/tiles/taxes.jpg'
-    },
-    {
-      id: '8',
-      image: 'src/assets/img/tiles/traditions.jpg'
-    },
-    {
-      id: '9',
-      image: 'src/assets/img/tiles/values.jpg'
-    },
-    {
-      id: '10',
-      image: 'src/assets/img/tiles/work.jpg'
-    },
-    {
-      id: '1',
-      image: 'src/assets/img/tiles/'
-    },
-  ];
+
 
   constructor() {
   }
--- information-belgium/src/app/containers/tiles-grid/tiles-grid.component.ts
+++ information-belgium/src/app/containers/tiles-grid/tiles-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-tiles-grid',
@@ -7,7 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TilesGridComponent implements OnInit {
 
-  constructor() { }
+  public infoTopics = [
+    {
+      id: '1',
+      image: 'education.jpg'
+    },
+    {
+      id: '2',
+      image: 'healthcare.jpg'
+    },
+    {
+      id: '3',
+      image: 'history.jpg'
+    },
+    {
+      id: '4',
+      image: 'housing.jpg'
+    },
+    {
+      id: '5',
+      image: 'politics.jpg'
+    },
+    {
+      id: '6',
+      image: 'recycling.jpg'
+    },
+    {
+      id: '7',
+      image: 'taxes.jpg'
+    },
+    {
+      id: '8',
+      image: 'traditions.jpg'
+    },
+    {
+      id: '9',
+      image: 'values.jpg'
+    },
+    {
+      id: '10',
+      image: 'work.jpg'
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
