01e65c96b0d4e2c79764cbab80264c4fe3e873b4
Joel Camacho <joel.camacho@icometrix.com>
2020-11-26 09:59:41 +0100

    back button

--- information-belgium/src/app/containers/sector-list/sector-list.component.ts
+++ information-belgium/src/app/containers/sector-list/sector-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import * as dataResources from '../../data/dataResources';
 import {ActivatedRoute} from '@angular/router';
 import {ResourcePortal} from '../../models';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-sector-list',
@@ -13,13 +14,18 @@ export class SectorListComponent implements OnInit {
   public filteredDataResources: ResourcePortal[];
   public firstDataResource: ResourcePortal;
 
-  constructor(public route: ActivatedRoute) {
+  constructor(public route: ActivatedRoute,
+              public location: Location) {
   }
 
   ngOnInit() {
     this.filteredDataResources = dataResources.dataResources.filter(resource => resource.sector === this.route.snapshot.params.sectorId);
     window.scroll(0, 0);
     this.firstDataResource = this.filteredDataResources[0];
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
