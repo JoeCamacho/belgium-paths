506a81b97c34e3e0f84a66ef97b16fc02598efeb
Joel Camacho <joel.camacho@icometrix.com>
2021-01-08 22:08:53 +0100

    add packages for translations

--- information-belgium/src/app/app.module.ts
+++ information-belgium/src/app/app.module.ts
@@ -19,6 +19,17 @@ import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient} from '@angular/common/http';
+import {LocationStrategy} from '@angular/common';
+import {MultiTranslateHttpLoader} from 'ngx-translate-multi-http-loader';
+
+export function createTranslateLoader(http: HttpClient, locationStrategy: LocationStrategy) {
+  const href = locationStrategy.getBaseHref() !== '/' ? locationStrategy.getBaseHref() : '';
+  return new MultiTranslateHttpLoader(http, [
+    {prefix: `${href}/assets/i18n/app/`, suffix: '.json'},
+  ]);
+}
 
 const MaterialModules = [
   MatIconModule,
@@ -37,6 +48,15 @@ const MaterialModules = [
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
+
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient, LocationStrategy],
+      },
+      isolate: true,
+    }),
 
     ...MaterialModules,
   ],
