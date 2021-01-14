9d7bd47882abc83e8d4c4610e575afe5cd5e83ed
Joel Camacho <joel.camacho@icometrix.com>
2021-01-14 22:55:57 +0100

    app name refactoring

--- belgium-paths/karma.conf.js
+++ belgium-paths/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/information-belgium'),
+      dir: require('path').join(__dirname, './coverage/belgium-paths'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
